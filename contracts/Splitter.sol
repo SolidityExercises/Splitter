0b02c9d123a0229cc8dbf0fd285118d6eb1fcdba
Yoskata <root@localhost.localdomain>
2018-05-09 06:17:46 -0400

    Fixced spacings

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -4,21 +4,21 @@ import '../node_modules/zeppelin-solidity/contracts/math/SafeMath.sol';
 import '../node_modules/zeppelin-solidity/contracts/lifecycle/Destructible.sol';
 
 contract Splitter is Destructible {
-    using SafeMath for uint256;
+        using SafeMath for uint256;
 
-    event RecipientAdded(address indexed holder, address indexed recipient);
+        event RecipientAdded(address indexed holder, address indexed recipient);
 	event SplitPerformed(address indexed holder, uint256 amountToSplit);
 	event WithdrawalPerformed(address indexed recipient, uint256 amountToWithdrawal);
 
-    uint8 public constant MAX_RECIPIENTS = 254;
+        uint8 public constant MAX_RECIPIENTS = 254;
 
-    struct Recipients {
-        address[] recipients;
-        mapping(address => bool) allowed;
-    }
+        struct Recipients {
+                address[] recipients;
+                mapping(address => bool) allowed;
+        }
 
-    mapping (address => uint256) public balances;
-    mapping (address => Recipients) holders;
+        mapping (address => uint256) public balances;
+        mapping (address => Recipients) holders;
 
 	modifier allowedRecipientsCount() {
 		require(getRecipientsCount(msg.sender) < MAX_RECIPIENTS);
@@ -56,41 +56,41 @@ contract Splitter is Destructible {
 		return holders[_holder].recipients.length;
 	}
 
-    function addRecipient(address _recipientAddress) public allowedRecipientsCount onlyIfNewRecipient(_recipientAddress) {
-        require(_recipientAddress != address(0));
-        require(_recipientAddress != msg.sender);
+        function addRecipient(address _recipientAddress) public allowedRecipientsCount onlyIfNewRecipient(_recipientAddress) {
+                require(_recipientAddress != address(0));
+                require(_recipientAddress != msg.sender);
 
-        holders[msg.sender].allowed[_recipientAddress] = true;
-        holders[msg.sender].recipients.push(_recipientAddress);
+                holders[msg.sender].allowed[_recipientAddress] = true;
+                holders[msg.sender].recipients.push(_recipientAddress);
 
-        emit RecipientAdded(msg.sender, _recipientAddress);
-    }
-
-    function split() public payable onlyIfAmountEnoughForSplit {
-        require(getRecipientsCount(msg.sender) > 0);
-
-        uint256 reminder = msg.value % holders[msg.sender].recipients.length;
-        
-        if(reminder != 0){
-            balances[msg.sender] = balances[msg.sender].add(reminder);
-        }
-        
-        uint256 splittedAmount = msg.value.div(holders[msg.sender].recipients.length);
-
-        address recipientAddress;
-        for(uint8 i = 0; i < holders[msg.sender].recipients.length; i++){
-            recipientAddress = holders[msg.sender].recipients[i];
-            balances[recipientAddress] = balances[recipientAddress].add(splittedAmount);
+                emit RecipientAdded(msg.sender, _recipientAddress);
         }
 
-        emit SplitPerformed(msg.sender, splittedAmount);
-    }
+        function split() public payable onlyIfAmountEnoughForSplit {
+                require(getRecipientsCount(msg.sender) > 0);
 
-    function withdrawal(uint256 _amount) public onlyIfBalanceEnough(_amount) {
-        balances[msg.sender] = balances[msg.sender].sub(_amount);
+                uint256 reminder = msg.value % holders[msg.sender].recipients.length;
+        
+                if(reminder != 0){
+                        balances[msg.sender] = balances[msg.sender].add(reminder);
+                }
+        
+                uint256 splittedAmount = msg.value.div(holders[msg.sender].recipients.length);
 
-        msg.sender.transfer(_amount);
+                address recipientAddress;
+                for(uint8 i = 0; i < holders[msg.sender].recipients.length; i++){
+                        recipientAddress = holders[msg.sender].recipients[i];
+                        balances[recipientAddress] = balances[recipientAddress].add(splittedAmount);
+                }
 
-        emit WithdrawalPerformed(msg.sender, _amount);
-    }
+                emit SplitPerformed(msg.sender, splittedAmount);
+       }
+
+        function withdrawal(uint256 _amount) public onlyIfBalanceEnough(_amount) {
+                balances[msg.sender] = balances[msg.sender].sub(_amount);
+
+                msg.sender.transfer(_amount);
+
+                emit WithdrawalPerformed(msg.sender, _amount);
+        }
 }
